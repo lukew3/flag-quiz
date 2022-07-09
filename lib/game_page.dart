436eef77bf2aff3af7a06c17cbff6f65d85e5d53
lukew3 <lukew25073@gmail.com>
2022-07-08 23:40:34 -0400

    game page styling

--- lib/game_page.dart
+++ lib/game_page.dart
@@ -3,56 +3,83 @@ import 'package:flutter/material.dart';
 class GamePage extends StatelessWidget {
   const GamePage({Key? key}) : super(key: key);
 
+  submitGuess(String guess) {
+    print("Guess: $guess");
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: <Widget>[
+      body: Column(
+        children: <Widget>[
           Padding(
-              padding: const EdgeInsets.all(20),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: const <Widget>[
-                  Text('Flag Quiz',
-                      style: TextStyle(color: Colors.white, fontSize: 20)),
-                  Text('Score: 0',
-                      style: TextStyle(color: Colors.white, fontSize: 20)),
-                ],
-              )),
-          const Center(
-              child: Image(
-            image: AssetImage('assets/flag_usa.png'),
-            width: 500,
-          )),
-          TextField(
-            decoration: const InputDecoration(
-              labelText: 'Country Name',
-              labelStyle: TextStyle(color: Colors.white),
-            ),
-            style: const TextStyle(color: Colors.white),
-            onSubmitted: (value) {
-              print('Submitted: $value');
-            },
-          ),
-          /*
-          Row(
-            children: [
-              const TextField(
-                decoration: InputDecoration(
-                  labelText: 'Enter your name',
-                  labelStyle: TextStyle(color: Colors.white),
+            padding: const EdgeInsets.all(20),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: const <Widget>[
+                Text(
+                  'Flag Quiz',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20,
+                  ),
                 ),
+                Text(
+                  'Score: 0',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          const Center(
+            child: Padding(
+              padding: EdgeInsets.all(60),
+              child: Image(
+                image: AssetImage('assets/flag_usa.png'),
+                width: 500,
               ),
-              TextButton(
-                  onPressed: () {},
-                  child: const Text(
-                    'Submit',
-                    style: TextStyle(color: Colors.white),
-                  ))
-            ],
-          )
-          */
-        ]));
+            ),
+          ),
+          Center(
+            child: SizedBox(
+              width: 500,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  SizedBox(
+                    width: 400,
+                    child: TextField(
+                      decoration: const InputDecoration(
+                        labelText: 'Country Name',
+                        labelStyle: TextStyle(color: Colors.grey),
+                      ),
+                      style: const TextStyle(color: Colors.white),
+                      onSubmitted: submitGuess,
+                    ),
+                  ),
+                  TextButton(
+                    style: TextButton.styleFrom(
+                      primary: Colors.black,
+                      backgroundColor: Colors.green,
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 10,
+                        vertical: 20,
+                      ),
+                    ),
+                    onPressed: () {},
+                    child: const Text(
+                      'Submit',
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
