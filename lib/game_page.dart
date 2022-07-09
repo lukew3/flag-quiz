8a91c67605c44a9781c29ac02d6ca9c47e542b29
lukew3 <lukew25073@gmail.com>
2022-07-09 00:50:45 -0400

    Add review page to gamepage

--- lib/game_page.dart
+++ lib/game_page.dart
@@ -1,81 +1,171 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 
-class GamePage extends StatelessWidget {
+class GamePage extends StatefulWidget {
   const GamePage({Key? key}) : super(key: key);
+  @override
+  _GamePageState createState() => _GamePageState();
+}
 
-  submitGuess(String guess) {
+class _GamePageState extends State<GamePage> {
+  int _questionNumber = 0;
+  bool _guessing = true;
+
+  _submitGuess(String guess) {
     print("Guess: $guess");
+    setState(() {
+      _guessing = false;
+    });
+  }
+
+  _nextGuess() {
+    print('Next guess');
+    setState(() {
+      _guessing = true;
+      _questionNumber++;
+    });
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
-        children: <Widget>[
-          Padding(
-            padding: const EdgeInsets.all(20),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: const <Widget>[
-                Text(
-                  'Flag Quiz',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 20,
-                  ),
+    if (_guessing) {
+      return Scaffold(
+        body: Column(
+          children: <Widget>[
+            const GameHeader(),
+            const Center(
+              child: Padding(
+                padding: EdgeInsets.all(60),
+                child: Image(
+                  image: AssetImage('assets/flag_usa.png'),
+                  width: 500,
                 ),
-                Text(
-                  'Score: 0',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 20,
-                  ),
-                ),
-              ],
+              ),
             ),
-          ),
-          const Center(
-            child: Padding(
-              padding: EdgeInsets.all(60),
-              child: Image(
-                image: AssetImage('assets/flag_usa.png'),
+            Center(
+              child: SizedBox(
                 width: 500,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    SizedBox(
+                      width: 400,
+                      child: TextField(
+                        decoration: const InputDecoration(
+                          labelText: 'Country Name',
+                          labelStyle: TextStyle(color: Colors.grey),
+                        ),
+                        style: const TextStyle(color: Colors.white),
+                        onSubmitted: _submitGuess,
+                      ),
+                    ),
+                    TextButton(
+                      style: TextButton.styleFrom(
+                        primary: Colors.black,
+                        backgroundColor: Colors.green,
+                        padding: const EdgeInsets.symmetric(
+                          horizontal: 10,
+                          vertical: 20,
+                        ),
+                      ),
+                      onPressed: () {},
+                      child: const Text(
+                        'Submit',
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ),
-          ),
-          Center(
-            child: SizedBox(
-              width: 500,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  SizedBox(
-                    width: 400,
-                    child: TextField(
-                      decoration: const InputDecoration(
-                        labelText: 'Country Name',
-                        labelStyle: TextStyle(color: Colors.grey),
-                      ),
-                      style: const TextStyle(color: Colors.white),
-                      onSubmitted: submitGuess,
-                    ),
+          ],
+        ),
+      );
+    } else {
+      return GestureDetector(
+        onTap: _nextGuess,
+        child: Scaffold(
+          body: Column(
+            children: const <Widget>[
+              GameHeader(),
+              Center(
+                child: Padding(
+                  padding: EdgeInsets.fromLTRB(0, 60, 0, 20),
+                  child: Image(
+                    image: AssetImage('assets/flag_usa.png'),
+                    width: 500,
                   ),
-                  TextButton(
-                    style: TextButton.styleFrom(
-                      primary: Colors.black,
-                      backgroundColor: Colors.green,
-                      padding: const EdgeInsets.symmetric(
-                        horizontal: 10,
-                        vertical: 20,
-                      ),
-                    ),
-                    onPressed: () {},
-                    child: const Text(
-                      'Submit',
-                    ),
-                  ),
-                ],
+                ),
               ),
+              Center(
+                child: Text(
+                  'United States of America',
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+              Center(
+                child: Padding(
+                  padding: EdgeInsets.fromLTRB(0, 80, 0, 20),
+                  child: Image(
+                    image: AssetImage('assets/correct.png'),
+                    width: 60,
+                  ),
+                ),
+              ),
+              Center(
+                child: Text(
+                  'United States of America is Correct!',
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+              Center(
+                child: Padding(
+                  padding: EdgeInsets.fromLTRB(0, 150, 0, 20),
+                  child: Text(
+                    'Press anywhere to continue...',
+                    style: TextStyle(
+                      fontSize: 20,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      );
+    }
+  }
+}
+
+class GameHeader extends StatelessWidget {
+  const GameHeader({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(20),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: const <Widget>[
+          Text(
+            'Flag Quiz',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 20,
+            ),
+          ),
+          Text(
+            '1/195',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 20,
             ),
           ),
         ],
