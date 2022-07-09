524b79da46263e54dd0a1d617338b07ac2b79cdb
lukew3 <lukew25073@gmail.com>
2022-07-08 23:42:09 -0400

    Improved home page code formatting

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -11,31 +11,46 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: <Widget>[
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
           const Center(
-              child: Text('Flag Quiz',
-                  style: TextStyle(
-                      fontSize: 32,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.white))),
+            child: Text(
+              'Flag Quiz',
+              style: TextStyle(
+                fontSize: 32,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+              ),
+            ),
+          ),
           const Center(
-              child: Image(image: AssetImage('assets/globe.gif'), height: 200)),
+            child: Image(
+              image: AssetImage('assets/globe.gif'),
+              height: 200,
+            ),
+          ),
           Center(
-              child: TextButton(
-                  style: TextButton.styleFrom(
-                      padding: const EdgeInsets.symmetric(
-                          vertical: 20, horizontal: 36),
-                      primary: Colors.black,
-                      backgroundColor: Colors.green),
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => const GamePage()),
-                    );
-                  },
-                  child: const Text('Start Quiz')))
-        ]));
+            child: TextButton(
+              style: TextButton.styleFrom(
+                padding: const EdgeInsets.symmetric(
+                  vertical: 20,
+                  horizontal: 36,
+                ),
+                primary: Colors.black,
+                backgroundColor: Colors.green,
+              ),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const GamePage()),
+                );
+              },
+              child: const Text('Start Quiz'),
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
